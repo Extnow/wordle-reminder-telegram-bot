1aa7c81a4ace4567fdc91de05ddedc42aee59f75
extnow <ryabushkin.mikhail@gmail.com>
2022-05-10 16:46:07 +0300

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+const TelegramApi = require('node-telegram-bot-api');
+const schedule = require('node-schedule');
+require('dotenv').config();
+
+let job;
+const bot = new TelegramApi(process.env.TOKEN, { polling: true });
+const reminderHour = 8;
+const reminderMinutes = 50;
+
+bot.on('message', msg => {
+  const { text, chat } = msg;
+  bot.sendMessage(
+    chat.id,
+    `Ссылка на новый wordle будет присылаться каждый день в ${reminderHour}:${reminderMinutes}, если хочешь сыграть сейчас, то - https://wordle.belousov.one/ 🤗`
+  );
+});
+
+bot.onText(/\/start/, message => {
+  job = schedule.scheduleJob(`${reminderMinutes} ${reminderHour} * * 0-6`, () => {
+    bot.sendMessage(message.chat.id, 'Новый wordle - https://wordle.belousov.one/');
+  });
+});
+
+bot.onText(/\/stop/, message => {
+  if (job) {
+    job.cancel();
+  }
+});
